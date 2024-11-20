ef8ecef6377f8fbad4d397536a1e2225041572e8
Konnor Rogers <konnor5456@gmail.com>
2024-11-19 20:57:35 -0500

    use anchors

--- examples/base/app/main.rb
+++ examples/base/app/main.rb
@@ -1,7 +1,6 @@
 def tick(args)
   text = "Hello World"
-  text_width, text_height = $gtk.calcstringbox(text)
-  args.outputs.labels << { x: (1280 / 2) - (text_width / 2), y: (720 / 2) - (text_height / 2), text: text }
+  args.outputs.labels << { x: (1280 / 2), y: (720 / 2), anchor_x: 0.5, anchor_y: 0.5, text: text }
 end
 
 $gtk.reset
