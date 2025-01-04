3198c6807d4db8a8329b6b478c255cfc5789223a
Konnor Rogers <konnor5456@gmail.com>
2025-01-04 15:38:41 -0500

    add render targets sample

--- examples/base/app/main.rb
+++ examples/base/app/main.rb
@@ -1,7 +1,24 @@
+PALETTE = {
+  red: { r: 255, g: 0, b: 0, a: 255 },
+  blue: { r: 0, g: 0, b: 255, a: 255 },
+}
 def tick(args)
-  text = "Hello World"
-  args.outputs.labels << { x: (1280 / 2), y: (720 / 2), anchor_x: 0.5, anchor_y: 0.5, text: text }
+  box = {
+    w: 50,
+    h: 50,
+    path: :pixel
+  }
+  red_box = box.merge({
+    x: 100,
+    y: 100,
+    **PALETTE.red,
+  })
+  blue_box = box.merge({
+    x: 200,
+    y: 200,
+    **PALETTE.blue
+  })
+  args.outputs.sprites << [red_box, blue_box]
 end
-
 $gtk.reset
 
