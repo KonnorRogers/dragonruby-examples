764d643ff5e8afe07502c8bf3efbc8e5d728c023
Konnor Rogers <konnor5456@gmail.com>
2025-01-04 22:05:50 -0500

    commit palettes

--- examples/base/app/main.rb
+++ examples/base/app/main.rb
@@ -1,24 +1,9 @@
-PALETTE = {
-  red: { r: 255, g: 0, b: 0, a: 255 },
-  blue: { r: 0, g: 0, b: 255, a: 255 },
-}
 def tick(args)
-  box = {
-    w: 50,
-    h: 50,
-    path: :pixel
+  args.outputs.labels << {
+    x: 640,
+    y: 360,
+    text: "Hello World"
   }
-  red_box = box.merge({
-    x: 100,
-    y: 100,
-    **PALETTE.red,
-  })
-  blue_box = box.merge({
-    x: 200,
-    y: 200,
-    **PALETTE.blue
-  })
-  args.outputs.sprites << [red_box, blue_box]
 end
 $gtk.reset
 
